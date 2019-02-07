0ea9a98a868434aec6900b3e0884b708aee26ae4
eddy <eddy@ualberta.ca>
2019-02-07 13:19:53 -0700

    refactored code

--- A1/src/cpsc441/a1/QuickUrl.java
+++ A1/src/cpsc441/a1/QuickUrl.java
@@ -1,12 +1,25 @@
 package cpsc441.a1;
 
+import java.net.Socket;
 import java.util.ArrayList;
 
 public class QuickUrl extends ConcurrentHttp 
 {
 	public void getObject(String s) 
 	{
-		//arraylist that stores all the threads
+		//parse URL into host, port, and pathname
+		String[] parsedURL = new Parser().parseURL(s);
+		String host = parsedURL[0];
+		int port = Integer.parseInt(parsedURL[1]);
+		String pathname = parsedURL[2];
+		
+		//send head request
+		int contentLength = new HeadRequest().getContentLength(pathname, port, host);
+		
+		//send range request
+		Socket socket = new RangeRequest().send(pathname, port, host, contentLength);
+		
+		//create a arraylist that stores all the threads
 		ArrayList<Thread> threads = new ArrayList<Thread>();
 		
 		//get number of connections needed
@@ -15,7 +28,7 @@ public class QuickUrl extends ConcurrentHttp
 		//if only one connection use a single thread
 		if(numConnection == 1)
 		{
-			Thread t1 = new Thread(new concurrentConnection(s));
+			Thread t1 = new Thread(new concurrentConnection(pathname, host, contentLength, socket));
 			t1.start();
 		}
 		//else create multiple threads
@@ -24,7 +37,7 @@ public class QuickUrl extends ConcurrentHttp
 			//create one thread for each connection
 			for(int i = 0; i < numConnection; i++)
 			{
-				Thread t = new Thread(new concurrentConnection(s));
+				Thread t = new Thread(new concurrentConnection(pathname, host, contentLength, socket));
 				threads.add(t);
 				//t.start();
 			}
--- A1/src/cpsc441/a1/concurrentConnection.java
+++ A1/src/cpsc441/a1/concurrentConnection.java
@@ -3,28 +3,22 @@ package cpsc441.a1;
 import java.net.Socket;
 
 public class concurrentConnection implements Runnable 
-{
-	String URL;
+{	
+	String pathname;
+	String host;
+	int contentLength;
+	Socket socket;
 	
-	public concurrentConnection(String s)
+	public concurrentConnection(String pathname, String host, int contentLength, Socket socket)
 	{
-		URL = s;
+		this.pathname = pathname;
+		this.host = host;
+		this.contentLength = contentLength;
+		this.socket = socket;
 	}
 	
 	public void run()
 	{
-		//parse URL into host, port, and pathname
-		String[] parsedURL = new Parser().parseURL(URL);
-		String host = parsedURL[0];
-		int port = Integer.parseInt(parsedURL[1]);
-		String pathname = parsedURL[2];
-		
-		//send head request
-		int contentLength = new HeadRequest().getContentLength(pathname, port, host);
-		
-		//send range request
-		Socket socket = new RangeRequest().send(pathname, port, host, contentLength);
-		
 		//download file
 		FileDownloader http = new FileDownloader();
 		http.download(pathname, host, contentLength, socket);
